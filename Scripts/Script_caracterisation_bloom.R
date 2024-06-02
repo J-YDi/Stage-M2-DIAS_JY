91a9f5ec534692e8af0da12919f4448a0f6daa05
J-YDi <157480680+J-YDi@users.noreply.github.com>
2024-06-02 21:15:57 +0200

    Add files via upload

--- Scripts/Script_caracterisation_bloom.R
+++ Scripts/Script_caracterisation_bloom.R
@@ -1,9 +1,12 @@
+# Script JY.Dias - Stage M2 #
+
 # Load packages
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(trend)
 library(lubridate)
+library(cowplot)
 
 ##########################################
 # Table_bloom_R_v3c.csv was made by hand #
@@ -23,33 +26,6 @@ data_bloom$Monodominance <- ifelse(data_bloom$P_dominance >= median(data_bloom$P
 
 write.csv2(data_bloom,file="data_modif/Table_bloom_VF_final.csv", row.names = FALSE,dec = ".")
 
-# Trend number of blooms over time ####
-data_bloom_trend <- read_delim("data_modif/Trend_bloom_md.csv", 
-                         delim = ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"),
-                         trim_ws = TRUE)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "1")$N_bloom)
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "1")$P_MD)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "2")$N_bloom) #SIGN
-sens.slope(dplyr::filter(data_bloom_trend, Cluster == "2")$N_bloom)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "2")$P_MD)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "3")$N_bloom)
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "3")$P_MD)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "4")$N_bloom) #SIGN
-sens.slope(dplyr::filter(data_bloom_trend, Cluster == "4")$N_bloom)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "4")$P_MD) #SIGN
-sens.slope(dplyr::filter(data_bloom_trend, Cluster == "4")$P_MD)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "Tous")$N_bloom) #SIGN
-sens.slope(dplyr::filter(data_bloom_trend, Cluster == "Tous")$N_bloom)
-
-mk.test(dplyr::filter(data_bloom_trend, Cluster == "Tous")$P_MD)
-
 # Difference on percentage of dominance during between bloom and no bloom ####
 
 data_bloom <- read_delim("data_modif/Table_bloom_R_v3c.csv", 
@@ -61,18 +37,76 @@ data <- read_delim("data_modif/Table_FLORTOT_Surf_0722_COM_period_Stselect_hydro
                    delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",", 
                                                                        grouping_mark = ""), trim_ws = TRUE)
 
-data$Abdtot <- apply(data[,24:328],1,sum,na.rm=T)
+data <- dplyr::select(data, Code_point_Libelle,cluster, Date,Month, BergerParker)
 
-data$Abdmax <- apply(data[,24:328],1,max,na.rm=T)
-
-data$P_dominance <- data$Abdmax/data$Abdtot 
-
-data <- dplyr::select(data, Code_point_Libelle,cluster, Date, P_dominance)
-
-data_bloom <- dplyr::select(data_bloom, -cluster)
+data_bloom <- dplyr::select(data_bloom, -cluster,-BergerParker,-Month)
 
 data_combo <- left_join(data,data_bloom, by = join_by(Code_point_Libelle, Date))
 
-wilcox.test(filter(data_combo, is.na(Bloom_Phylum))$P_dominance.x,
-            filter(data_combo, Month >= 0)$P_dominance.x)
+data_combo <- data_combo |>
+  mutate(season = case_when(Month %in% c(12, 01, 02) ~ "Winter",
+                            Month %in% c(03, 04, 05) ~ "Spring",
+                            Month %in% c(06, 07, 08) ~ "Summer",
+                            Month %in% c(09, 10, 11) ~ "Fall", TRUE ~ NA_character_))
+set.seed(123)
+nbhiverclus1 <- sample_n(filter(data_combo,season == "Winter" & cluster == 1 & is.na(Bloom_Phylum)),21)
+nbhiverclus2 <- sample_n(filter(data_combo,season == "Winter" & cluster == 2 & is.na(Bloom_Phylum)),3)
+nbhiverclus3 <- sample_n(filter(data_combo,season == "Winter" & cluster == 3 & is.na(Bloom_Phylum)),3)
+
+nbautomneclus1 <- sample_n(filter(data_combo,season == "Fall" & cluster == 1 & is.na(Bloom_Phylum)),28)
+nbautomneclus2 <- sample_n(filter(data_combo,season == "Fall" & cluster == 2 & is.na(Bloom_Phylum)),9)
+nbautomneclus3 <- sample_n(filter(data_combo,season == "Fall" & cluster == 3 & is.na(Bloom_Phylum)),11)
+
+nbspringclus1 <- sample_n(filter(data_combo,season == "Spring" & cluster == 1 & is.na(Bloom_Phylum)),29)
+nbspringclus2 <- sample_n(filter(data_combo,season == "Spring" & cluster == 2 & is.na(Bloom_Phylum)),49)
+nbspringclus3 <- sample_n(filter(data_combo,season == "Spring" & cluster == 3 & is.na(Bloom_Phylum)),61)
+
+nbeteclus1 <- sample_n(filter(data_combo,season == "Summer" & cluster == 1 & is.na(Bloom_Phylum)),22)
+nbeteclus2 <- sample_n(filter(data_combo,season == "Summer" & cluster == 2 & is.na(Bloom_Phylum)),39)
+nbeteclus3 <- sample_n(filter(data_combo,season == "Summer" & cluster == 3 & is.na(Bloom_Phylum)),25)
+
+data_hasard <- bind_rows(nbhiverclus1,nbhiverclus2,nbhiverclus3,nbautomneclus1,nbautomneclus2,nbautomneclus3,nbspringclus1,nbspringclus2,nbspringclus3,
+                         nbeteclus1,nbeteclus2,nbeteclus3)
+
+data_bloom <- filter(data_combo, !is.na(Bloom_Phylum))
+
+data_combo <- bind_rows(data_bloom,data_hasard)
+
+data_combo$TBloom <- ifelse(is.na(data_combo$Bloom),"Non-bloom","Bloom")
+
+data_combo$cluster[data_combo$cluster == "1"] <- "1-Méditerranée"
+data_combo$cluster[data_combo$cluster == "2"] <- "2-Manche orientale - Mer du Nord"
+data_combo$cluster[data_combo$cluster == "3"] <- "3-Atlantique - Manche occidentale"
+data_combo$cluster[data_combo$cluster == "4"] <- "4-Mer des Pertuis"
+data_combo$cluster <- as.factor(data_combo$cluster)
+
+# Color for each cluster
+cluster_col <- c("1-Méditerranée" = "#F8766D","2-Manche orientale - Mer du Nord" = "#CD9600", 
+                 "3-Atlantique - Manche occidentale" = "#00BE67",  "4-Mer des Pertuis" = "#00A9FF")
+
+BKglobal <- ggplot(filter(data_combo, cluster != "4-Mer des Pertuis"))+
+  geom_boxplot(aes(x=TBloom,y=BergerParker,group=TBloom))+
+  labs(x="Evènement",y="Indice de Berger-Parker")
+
+BKregion <- ggplot(filter(data_combo, cluster !="4-Mer des Pertuis"))+
+  geom_boxplot(aes(x=TBloom,y=BergerParker,group=TBloom,fill=as.character(cluster)))+
+  labs(x="Evènement",y="Indice de Berger-Parker")+
+  scale_fill_manual(values=cluster_col,guide = "none")+
+  facet_wrap(~cluster)
+
+data_combo$Bloom_Phylum[is.na(data_combo$Bloom_Phylum)] <- "Non-bloom"
+
+
+BKdetail <- ggplot(filter(data_combo, cluster != "4-Mer des Pertuis", Bloom_Phylum == "Bac" | Bloom_Phylum == "Dino" | Bloom_Phylum == "Non-bloom"))+
+  geom_boxplot(aes(x=Bloom_Phylum,y=BergerParker,group=Bloom_Phylum,fill=as.character(cluster)))+
+  labs(x="Evènement",y="Indice de Berger-Parker")+
+  scale_fill_manual(values=cluster_col,guide = "none")+
+  facet_wrap(~cluster,scales = "free_x")
+
+plot_grid(BKregion,BKdetail,ncol=1,labels = "AUTO")
+
+wilcox.test(filter(data_combo,TBloom == "Bloom")$BergerParker,filter(data_combo,TBloom != "Bloom")$BergerParker)
+wilcox.test(filter(data_combo,Bloom_Phylum == "Dino" & cluster == "1-Méditerranée")$BergerParker,filter(data_combo,TBloom != "Bloom" & cluster == "1-Méditerranée")$BergerParker)
+wilcox.test(filter(data_combo,Bloom_Phylum == "Dino" & cluster == "2-Manche orientale - Mer du Nord")$BergerParker,filter(data_combo,TBloom != "Bloom" & cluster == "2-Manche orientale - Mer du Nord")$BergerParker)
+wilcox.test(filter(data_combo,Bloom_Phylum == "Dino" & cluster == "3-Atlantique - Manche occidentale")$BergerParker,filter(data_combo,TBloom != "Bloom" & cluster == "3-Atlantique - Manche occidentale")$BergerParker)
 
